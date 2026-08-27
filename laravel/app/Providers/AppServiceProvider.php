951e1db2fb9a0e1e66474e7816c296e44f0176b8
NTN RANDRI <randrianambinintsoa43@gmail.com>
2026-08-27 10:16:16 +0300

    Laravel

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
+
         ResetPassword::createUrlUsing(function ($user, string $token) {
 
         return env('FRONTEND_URL')
