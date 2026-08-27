69200d738a0882486efdbe6c74d7da6c737db01e
NTN RANDRI <randrianambinintsoa43@gmail.com>
2026-08-27 10:14:21 +0300

    Laravel: correction create users table

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('nom');
             $table->string('prenom');
-            $table->string('email')->unique();
+            $table->string('email', 191)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
